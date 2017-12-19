2f2453e2e19a93a281bb2d52fac599740611c02e
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2017-12-19 17:00:21 +0530

    commit - display for messges from db

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { ChatMessage } from '../modals/chat-message.modal';
+
+import { ChatService } from './../services/chat.service';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  chatMessages: ChatMessage[];
+  isLoading: boolean = true;
+
+  constructor(
+    private chatService: ChatService
+  ) { }
 
   ngOnInit() {
+    this.chatService.getEarlierMessages().subscribe((res) => {
+      this.chatMessages = res;
+      this.isLoading = false;
+    });
+  }
+
+  ngOnChanges() {
+    this.chatService.getEarlierMessages().subscribe((res) => {
+      this.chatMessages = res;
+      this.isLoading = false;
+    });
   }
 
 }
--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChatMessage } from '../modals/chat-message.modal';
+import { AuthService } from './../services/auth.service';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  @Input('message') message: ChatMessage;
+
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
+    this.authService.authUser().subscribe((res) => {
+      if (res) {
+        this.message.fromMe = this.message.email === res.email ? true : false;
+      }
+    });
   }
 
 }
